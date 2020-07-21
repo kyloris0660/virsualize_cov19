2d6afce0a0306b3ecbcd69f8508fb5dbe33dfe8b
kyloris0660 <1489821378@qq.com>
2020-07-21 22:20:05 +0800

    add infection

--- app.py
+++ app.py
@@ -59,7 +59,32 @@ def city_info():  # 默认返回内容
     get_Data = json.loads(get_Data)
     prem = get_Data.get('key')
     # prem = "'上海'"
-    str1 = 'select * from summer.population where province_name=' + ('\'') + prem + ('\'')
+    str1 = 'select * from summer.population where province_name=' + ('\'') + prem + ('\';')
+    print(str1)
+    db = SQLManager()
+    temp = db.get_list(str1)
+    print(temp)
+    db.close()
+    return json.dumps(temp, ensure_ascii=False)
+
+
+@app.route("/infection", methods=["POST", "GET"])
+def china_provincedata():  # 默认返回内容
+    return_dict = {'return_code': 200, 'return_info': '处理成功', 'result': False}
+
+    # 判断传入的json数据是否为空
+    if request.get_data() is None:
+        return_dict['return_code'] = '5004'
+        return_dict['return_info'] = '请求参数为空'
+        return json.dumps(return_dict, ensure_ascii=False)
+
+    # 获取传入的参数
+    get_Data = request.get_data()
+    get_Data = json.loads(get_Data)
+    prem = get_Data.get('key')
+
+    str1 = 'SELECT confirmedCount, confirmedIncr, dateId FROM summer.china_provincedata where provinceShortName=' + (
+        '\'') + prem + ('\';')
     print(str1)
     db = SQLManager()
     temp = db.get_list(str1)
