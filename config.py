35bb34de6b0ba220c8a2104c3ca643142c9a0dd7
kyloris0660 <1489821378@qq.com>
2020-07-21 22:08:52 +0800

    init2.

--- config.py
+++ config.py
@@ -1,13 +1,5 @@
 import pymysql
 from db_config import *
-DB_CONFIG = {
-    "host": "127.0.0.1",
-    "port": 3306,
-    "user": "root",
-    "passwd": "kevin660",
-    "db": "summer",
-    "charset": "utf8"
-}
 
 
 class SQLManager(object):
